5131ffe997c3a9ca0499c129be3e85406a45b35e
philokawi <philokawi@gmail.com>
2022-07-01 02:15:25 -0700

    size

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main -Entry point
+ * Return:Always 0 (success)
+ */
+int main(void)
+{
+int i;
+long int | ;
+long long int || ;
+float d;
+char c;
+printf("Size of a char: %lu byte...
