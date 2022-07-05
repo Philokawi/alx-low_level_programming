296b28fc9ae97e6e3034cd5e26609ac5f00905e9
philokawi <philokawi@gmail.com>
2022-07-05 10:51:57 -0700

    fix islower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,5 +1,6 @@
 #include "main.h"
 /**
+ * main - check code
  *Description: _is lower -> check if the character is lower
  * @c: a character arguement
  *Return: return 1 and 0 depending on condition
