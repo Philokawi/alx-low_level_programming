699b37b85e7254affbf0d3ed39de7eed16118665
philokawi <philokawi@gmail.com>
2022-07-11 05:28:02 -0700

    reverse string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,35 @@
+#include <string.h>
+#include "main.h"
+/**
+* rev_string - reverses string in place, without printing it
+* @s: string to reverse
+* Return: void
+*/
+
+void rev_string(char *s)
+
+{
+
+int i, len;
+
+char a, z;
+
+len = _strlen(s) - 1;
+
+i = 0;
+
+while (i < len)
+
+{
+
+a = s[i];
+
+z = s[len];
+
+s[i++] = z;
+
+s[len--] = a;
+
+}
+
+}
